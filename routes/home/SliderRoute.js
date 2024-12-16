11e2865570a70a0e9a3ffe2b04a386ceced01d26
wijayakaa <kolekmemilihmurtad@gmail.com>
2024-12-16 10:12:05 +0700

    feature: [BW] slider-and-any-fix

--- routes/home/SliderRoute.js
+++ routes/home/SliderRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { getSlider, createSlider, updateSlider, deleteSlider } from "../../controllers/home/Slider.js";
+import {getSlider, createSlider, updateSlider, deleteSlider} from "../../controllers/home/Slider.js";
 import { uploadSlider, validateFileType } from "../../middleware/uploadFile.js";
 import { verifyAdmin } from "../../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get("/slider", getSlider);
+router.get("/slider",getSlider );
 router.post("/slider", verifyAdmin, uploadSlider.single("image"), validateFileType, createSlider);
-router.patch("/slider/:id", verifyAdmin, uploadSlider.single("image"), validateFileType, updateSlider)
-router.delete("/slider/:id", verifyAdmin, deleteSlider);
+router.patch("/slider/:id", verifyAdmin, uploadSlider.single("image"), validateFileType,updateSlider )
+router.delete("/slider/:id", verifyAdmin,deleteSlider );
 
 export default router;
