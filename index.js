12e014a36d348bb772185509ed75fc5bec9bb7af
wijayakaa <107695774+wijayakaa@users.noreply.github.com>
2025-01-20 14:50:43 +0700

    fix

--- index.js
+++ index.js
@@ -85,6 +85,10 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
 app.use('/uploads', express.static('public/uploads'));
 app.use(UserRoute);
 app.use(ProductRoute);
