84ac9282003b6142d9e06c04b64ba8363d416d50
wijayakaa <kolekmemilihmurtad@gmail.com>
2025-01-20 11:47:28 +0700

    fix: [BW] fix

--- config/Database.js
+++ config/Database.js
@@ -1,14 +1,18 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
     dialect: 'mysql',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    dialectModule: require('mysql2'), 
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
 export default db;
